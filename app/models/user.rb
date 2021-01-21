4e142f7de5b8663d51c829c0d53e41cab67d497d
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-21 19:18:09 +0900

    コードレビュー後の修正　１回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+  with_options presence: true do
+   validates :nickname
+   validates :family_name
+   validates :given_name
+   validates :family_name_kana 
+   validates :given_name_kana
+   validates :birthday         
+   validates :password
+ end 
+ 
+  with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: '全角文字を使用してください' } do
+   validates :family_name
+   validates :given_name
+  end
 
-  validates :nickname,         presence: true
-  validates :family_name,      presence: true,
-                               format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: '全角文字を使用してください' }
-  validates :given_name,       presence: true,
-                               format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: '全角文字を使用してください' }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
-  validates :given_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
-  validates :birthday,         presence: true
-  validates :password,         presence: true,
-                               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+   validates :family_name_kana
+   validates :given_name_kana
+  end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
 
   has_many :items
   has_many :purchases
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
+    it 'emailに@が含まれないと登録できない' do
+      @user.email = "testemail"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+
     it '重複したemailが存在する場合登録できない' do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
