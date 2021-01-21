f648eea18210cb309e8fb652b2a16cd35565c6c1
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-21 11:32:07 +0900

    新規登録時バリデーション詳細実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :family_name,      presence: true
-  validates :given_name,       presence: true
-  validates :family_name_kana, presence: true
-  validates :given_name_kana,  presence: true
+  validates :family_name,      presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "全角文字を使用してください" }
+  validates :given_name,       presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "全角文字を使用してください" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" }
+  validates :given_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" }
   validates :birthday,         presence: true
+  validates :password,         presence: true, format: { with: /\A[a-z0-9]+\z/{6,}, message: "半角英数字を使用してください" }
 
   has_many :items
   has_many :purchases
