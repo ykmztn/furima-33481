ce7205cd2fdb21ed208f3b59c0d677d500416b81
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-21 16:55:40 +0900

    userモデル　テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" }
   validates :given_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" }
   validates :birthday,         presence: true
-  validates :password,         presence: true, format: { with: /\A[a-z0-9]+\z/, message: "半角英数字を使用してください" }
+  validates :password,         presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "Include both letters and numbers" }
 
   has_many :items
   has_many :purchases
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    family_name {山田}
-    given_name {太郎}
-    family_name_kana {ヤマダ}
-    given_name_kana {タロウ}
-    birthday {"2000-01-01"}
+    password {Faker::Internet.password(min_length: 6)} 
+    password_confirmation { password }
+    family_name { '山田' }
+    given_name { '太郎' }
+    family_name_kana { 'ヤマダ' }
+    given_name_kana { 'タロウ' }
+    birthday { "2000-01-01" }
   end
 end
