dda874bc5a82d207e3f4b4384bc1e838185b0eca
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-22 11:40:29 +0900

    バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :item_status_id, :shipping_cost_id, :ship_from_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id 
+    validates :item_status_id 
+    validates :shipping_cost_id
+    validates :ship_from_id
+    validates :shipping_day_id
+    validates :price
+    validates :image
+  end
 end
