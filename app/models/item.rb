7272b04e8dbc3ec2160778e57cb07d68698541d2
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-22 18:43:42 +0900

    ActiveHash　発送までの日数

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :item_status
   belongs_to :shipping_cost
   belongs_to :ship_from
+  belongs_to :shipping_day
 
   belongs_to :user
 
@@ -25,6 +26,8 @@ class Item < ApplicationRecord
     validates :category_id
     validates :item_status_id
     validates :shipping_cost_id
+    validates :ship_from_id
+    validates :shipping_day_id
   end
 
 end
