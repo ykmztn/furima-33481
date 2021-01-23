a0df302d69c965a864adb3ce475358c8abc01ca5
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-23 15:57:29 +0900

    価格の詳細バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,4 +30,6 @@ class Item < ApplicationRecord
     validates :shipping_day_id
   end
 
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+                      format: { with: /\A[0-9]+\z/,  message: '半角数字を使用してください'}
 end
