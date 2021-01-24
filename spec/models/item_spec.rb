3cdbf18b2f2720bdf6222b94ab21cfb78674a30e
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-24 17:12:18 +0900

    修正１回目　priceのテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,13 +68,13 @@ RSpec.describe Item, type: :model do
       end
 
       it '¥300より安い値段では出品できない' do
-        @item.price = '288'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it '¥9,999,999より高い値段では出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
