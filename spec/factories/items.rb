67fd998614de3a1c2e8f32553f84be56ae2d78da
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-24 16:18:03 +0900

    商品出品機能のテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+   item_name        {"商品名"}
+   description      {Faker::Lorem.sentence}
+   category_id      {2}
+   item_status_id   {2}
+   shipping_cost_id {2}
+   ship_from_id     {2}
+   shipping_day_id  {2}
+   price            {5000}
+
+   association :user
+
+  after(:build) do |item|
+    item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
+end
 end
