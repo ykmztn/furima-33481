5a3c3197412568588b131b2f5293d925cc25ce74
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-24 18:02:41 +0900

    修正３回目　validates不要コード削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_cost_id
-    validates :ship_from_id
-    validates :shipping_day_id
     validates :price
     validates :image
   end
