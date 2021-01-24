d7d0fad369a23beab8a018a5be582043dc184ffa
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-24 17:11:24 +0900

    修正１回目　price表示のコード修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,16 +3,14 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
-
+    
   const addTaxDom = document.getElementById("add-tax-price");
     const addTaxValue = (Math.floor(inputValue * 0.1));
     addTaxDom.innerHTML = addTaxValue;
-    console.log(addTaxValue);
 
     const salesProfit = document.getElementById("profit");
     salesProfit.innerHTML = (Math.floor(inputValue - addTaxValue));
-    console.log(saleProfit);
+ 
 
   })
 });
