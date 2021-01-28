fccfd2330cfa17649644ad046af5c3588c121826
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-28 18:59:15 +0900

    ログイン・ログアウト状態による画面遷移分岐

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
+    redirect_to root_path if current_user == @item.user
     @purchase_address = PurchaseAddress.new
+
   end
 
   def create
@@ -15,8 +19,13 @@ class PurchasesController < ApplicationController
   end
 
   private
- def purchase_params
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  
+  def purchase_params
   params.require(:purchase_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :purchase_id)
- end
+  end
 
 end
