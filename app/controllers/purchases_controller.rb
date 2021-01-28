60d6eafc1b19347511c1379fa122dca9a8fbf7c4
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-28 16:14:07 +0900

    フォームの情報の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,22 @@
 class PurchasesController < ApplicationController
 
   def index
-    @purchase_address = PurchaseAddress.includes(:item, :user)
+    @purchase_address = PurchaseAddress.new
   end
 
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+     if @purchase_address.valid?
+      @purchase_address.save
+       redirect_to action: :index
+     else
+       redirect_to item_purchases_path
+     end
+  end
 
+  private
+ def purchase_params
+  params.require(:purchase_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :purchase_id)
+ end
 
-   
 end
