eddc34ee4f320030b6f98a7aea960e039f15709a
ykmztn <you_ki1119@yahoo.co.jp>
2021-01-30 19:27:28 +0900

    売却済み商品に対しての条件分岐設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || @item.purchase != nil
   end
 
   def update
